9570f24da96c3bb9c3d9b0541482d8699fe297b9
AigerimQ <55206421+AigerimQ@users.noreply.github.com>
2020-12-09 13:55:10 +0600

    Проверка - не работает аутентификация

---
+++ jobs/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(label='Имя', max_length=100)
+    last_name = forms.CharField(label='Фамилия', max_length=100)
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        if commit:
+            user.save()
+        return user
